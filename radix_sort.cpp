6c38bf46c9795a29a8856cb1904d608b6a600105
pokopokko <deep06112004@gmail.com>
2025-03-30 02:53:50 -0500

    radix sort sequential implementation complete

--- radix_sort.cpp
+++ radix_sort.cpp
@@ -1,46 +1,100 @@
+/*
+ * Compile: g++ -Wall radix_sort.cpp -o rsort
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
 #include <cmath>
 using namespace std;
 
-int getMax(int array[], int n)
-{
-    int max = array[0];
-    for (int i = 1; i < n; i++)
-    {
-        if (array[i] > max)
-        {
-            max = array[i];
-        }
-    }
-    return max;
-}
-void generateRandomArray(int *array, int arraySize, int maxDigit)
+
+inline void generateRandomArray(int *array, int arraySize, int maxVal)
 {
     srand(time(0));
     for (int i = 0; i < arraySize; i++)
     {
-        array[i] = rand() % (int)(pow(10,maxDigit)); 
+        array[i] = rand() % maxVal;
     }
+}
+
+inline void printArray(int *array, int arraySize)
+{
+    for (int i = 0; i < arraySize; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
+inline void countSort(int *inputArray, int inputArraySize, int *outArray, int digit)
+{
+    int countArray[10];
+    for (int i = 0; i < 10; i++)
+    {
+        countArray[i] = 0;
+    }
+
+    for (int i = 0; i < inputArraySize; i++)
+    {
+
+        countArray[(inputArray[i] / digit) % 10]++;
+    }
+    int sum = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        sum += countArray[i];
+        countArray[i] = sum;
+    }
+
+    for (int i = inputArraySize - 1; i >= 0; i--)
+    {
+
+        countArray[(inputArray[i] / digit) % 10]--;
+        int newIndex = countArray[(inputArray[i] / digit) % 10];
+        outArray[newIndex] = inputArray[i];
+    }
+    for (int i = 0; i < inputArraySize; i++)
+    {
+        inputArray[i] = outArray[i];
+    }
+
+
 }
 
 int main(int argc, char *argv[])
 {
 
-    if (argc != 2)
+    if (argc != 3)
     {
-        printf("Usage: %s <sizeOffArray> <maxDigit#>\n", argv[0]);
+        printf("Usage: %s <sizeOffArray> <#Digits>\n", argv[0]);
         return -1;
     }
+    
 
-    int arraySize = atoi(argv[1]);
-    int maxDigit = atoi(argv[2]) - 1;
-    int array[maxDigit];
+    int inputArraySize = atoi(argv[1]);
+    int maxDigit = atoi(argv[2]);
+    int inputArray[inputArraySize];
+    int outArray[inputArraySize];
+    int maxPossibleValue;
+    if(maxDigit > 9){
+        printf("Max Value = 2147483647. Integer cap reached, all digit values 10 and above are set to the integer cap.\n");
+        maxPossibleValue =  2147483647;
+    } else {
+        maxPossibleValue  = (int)(pow(10, maxDigit));
+        printf("Max Value: %d\n", maxPossibleValue);
+    }
 
-    generateRandomArray(array, arraySize, maxDigit);
+    generateRandomArray(inputArray, inputArraySize, maxPossibleValue);
 
-    int max = getMax(array, arraySize);
+
+    printf("Initial input:\n");
+    printArray(inputArray, inputArraySize);
+
+    for (long int i = 1; i < maxPossibleValue; i*=10)
+    {
+        countSort(inputArray, inputArraySize, outArray, i);
+    }
+    printf("Final Output:\n");
+    printArray(outArray, inputArraySize);
 }
-
-
