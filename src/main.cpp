4072790e8f8b75b922f3d6c2ef30a4d7ce3de565
Stattek <stattek1@pm.me>
2025-03-30 00:40:10 -0500

    Update main to use new functions for doing counting sort example.

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,63 @@
 #include <stdio.h>
 #include <iostream>
 #include "radix_sort.h"
+#include "counting_sort.h"
+#include <stdlib.h>
+#include <cmath>
+#include <iostream>
+
+/**
+ * @brief Generates a random array for sorting.
+ * @note The array MUST BE FREED by the user of this function.
+ *
+ * @param arrayLen The length of the array.
+ * @param maxDigit The maximum digit to generate.
+ *
+ * @returns The randomly generated array or `NULL` upon error.
+ */
+static int *generateRandomArray(int arrayLen, int maxDigit)
+{
+    int *array = (int *)malloc(sizeof(int) * arrayLen);
+
+    srand(time(0));
+    for (int i = 0; i < arrayLen; i++)
+    {
+        array[i] = rand() % (int)(pow(10, maxDigit));
+    }
+
+    return array;
+}
 
 int main(int argc, char *argv[])
 {
 
     if (argc != 2)
     {
-        printf("Usage: %s <sizeOffArray> <maxDigit#>\n", argv[0]);
-        return -1;
+        printf("Usage: %s <sizeOffArray> <maxDigit#>\n\n", argv[0]);
+        return 1;
     }
 
-    int arraySize = atoi(argv[1]);
+    int arrayLen = atoi(argv[1]);
     int maxDigit = atoi(argv[2]) - 1;
-    int array[maxDigit];
 
-    generateRandomArray(array, arraySize, maxDigit);
+    int *array = generateRandomArray(arrayLen, maxDigit);
+    if (!array)
+    {
+        fprintf(stderr, "ERROR: Could not generate starting array\n");
+        return 1;
+    }
+    printf("helloworld\n");
 
-    int max = getMax(array, arraySize);
+    int *outputArray = (int *)malloc(sizeof(int) * arrayLen);
+    if (!outputArray)
+    {
+        fprintf(stderr, "ERROR: Could not allocate output array\n");
+        return 1;
+    }
+
+    if (countingSort(array, arrayLen, outputArray))
+    {
+        fprintf(stderr, "ERROR: Could not perform counting sort\n");
+        return 1;
+    }
 }
