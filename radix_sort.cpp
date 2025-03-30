003caac5a0bf1a3eb623caa0cbd514d5aadf5011
pokopokko <deep06112004@gmail.com>
2025-03-30 03:14:26 -0500

    fixed typos in output

--- radix_sort.cpp
+++ radix_sort.cpp
@@ -63,7 +63,7 @@ int main(int argc, char *argv[])
 
     if (argc != 3)
     {
-        printf("Usage: %s <sizeOffArray> <#Digits>\n", argv[0]);
+        printf("Usage: %s <sizeOfArray> <#Digits>\n", argv[0]);
         return -1;
     }
 
