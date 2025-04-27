33b3f2a9177807c4bb6e3ce519c2680c40882027
Stattek <stattek1@pm.me>
2025-04-27 05:28:16 -0500

    Remove old stuff.

--- src/sort/include/counting_sort.h
+++
@@ -1,7 +0,0 @@
-#ifndef COUNTING_SORT_H
-#define COUNTING_SORT_H
-
-bool getMax(const int *array, const int arrayLen, const int digit, int *output);
-bool countingSort(int *array, const int arrayLen, int *outputArray, const int digit);
-
-#endif
--- src/sort/include/radix_sort.h
+++
@@ -1,9 +0,0 @@
-/**
- * Summary: Radix sort header.
- */
-#ifndef RADIX_SORT_H
-#define RADIX_SORT_H
-
-bool radixSort(int **array, const int arrayLen, const int maxDigit);
-
-#endif
